594ca0ef872020d418711e283bea8ca53f29b596
Pritam <pritamofficial@gmail.com>
2024-02-21 23:19:47 +0530

    login page changes

--- app/Views/component/back/header.php
+++ app/Views/component/back/header.php
@@ -3,15 +3,6 @@
         <div class="d-flex">
             <!-- LOGO -->
             <div class="navbar-brand-box">
-                <a href="index.html" class="logo logo-dark">
-                    <span class="logo-sm">
-                        <img src="<??>assets/images/logo.svg" alt="" height="22">
-                    </span>
-                    <span class="logo-lg">
-                        <img src="assets/images/logo-dark.png" alt="" height="17">
-                    </span>
-                </a>
-
                 <a href="index.html" class="logo logo-light">
                     <span class="logo-sm">
                         <img src="<?=base_url()?>back/images/logo.png" alt="" height="35">
